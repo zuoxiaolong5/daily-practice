ed958a159163e279f381dd3ae67a507c46af254b
zuoxiaolong5 <z_xlong@126.com>
2018-03-19 22:25:45 +0800

    插入排序

--- algorithms/src/main/java/com/alg/sort/InsertionSort.java
+++ algorithms/src/main/java/com/alg/sort/InsertionSort.java
@@ -1,5 +1,7 @@
 package com.alg.sort;
 
+import com.alibaba.fastjson.JSON;
+
 /**
  * Created by Administrator on 2018/3/18 0018.
  */
@@ -7,11 +9,26 @@ public class InsertionSort implements ISort{
 
     @Override
     public void sort(int[] source) {
-
+        for (int i = 1; i < source.length; i++) {
+            int tem = source[i];
+            int j = i - 1;
+            // 找到当前元素要插入的位置 -->j
+            for (; j >= 0; j--) {
+                // 移动元素
+                if (source[j] > tem) {
+                    source[j + 1] = source[j];
+                } else
+                    break;
+            }
+            source[j + 1] = tem;
+        }
     }
 
     public static void main(String[] args) {
-        int[] source = DataGenerator.getIntArray(20);
-        System.out.println();
+        InsertionSort sort = new InsertionSort();
+        int[] source = DataGenerator.getIntArray(10);
+        System.out.println(JSON.toJSONString(source));
+        sort.sort(source);
+        System.out.println(JSON.toJSONString(source));
     }
 }
