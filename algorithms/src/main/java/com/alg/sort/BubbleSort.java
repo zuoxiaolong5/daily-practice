77a47bb2ca30d15108d90fdb90837b07f42f1aab
zuoxiaolong4 <zuoxiaolong5@jd.com>
2018-03-23 15:42:24 +0800

    冒泡

--- algorithms/src/main/java/com/alg/sort/BubbleSort.java
+++ algorithms/src/main/java/com/alg/sort/BubbleSort.java
@@ -10,7 +10,15 @@ public class BubbleSort implements ISort{
     @Override
     public void sort(int[] data) {
         // 每次比较相邻两个,较大的一个放到右边 一次循环结束能找到最大的
-
+        for (int i = 0; i < data.length -1; i++) {
+            for (int j = 0; j < data.length - i - 1; j++) {
+                if (data[j] >= data[j + 1]) {
+                    int tem = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = tem;
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
