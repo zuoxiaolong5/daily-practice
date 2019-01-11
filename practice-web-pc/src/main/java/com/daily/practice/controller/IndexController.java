9f65b558f42fda0f20219cbbb8629574093405bd
xiaolongzuol <zuoxiaolongl@163.com>
2019-01-11 23:02:08 +0800

    mybatis + mvc

--- practice-web-pc/src/main/java/com/daily/practice/controller/IndexController.java
+++ practice-web-pc/src/main/java/com/daily/practice/controller/IndexController.java
@@ -3,14 +3,23 @@ package com.daily.practice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
+    @RequestMapping(value = "/test")
+    public ModelAndView test() {
+        System.out.println("qqqq");
+        ModelAndView modelAndView = new ModelAndView("test");
+        return modelAndView;
+    }
+
     @RequestMapping(value = "/index")
+    @ResponseBody
     public String index() {
         System.out.println("qqqq");
-
-        return "test";
+        return "index";
     }
 }
